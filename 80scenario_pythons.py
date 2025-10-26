afe82064fe08f5ada23f5b21ca8526e12ed82f77
swirvtheman <swirvtheman@gmail.com>
2025-10-26 22:37:35 +0100

    just more practice

---
+++ 80scenario_pythons.py
@@ -0,0 +1,31 @@
+# 80 pythons ovningar scenario based descriptions
+
+# --------------------------------------------------------
+# Math:
+# 1
+# fika_per_person = int(25)
+# three_people = 3
+# person = 1
+# print(str(fika_per_person * three_people) +
+#      " and " + str(fika_per_person * person))
+
+# --------------------------------------------------------
+# 2
+# trip_time_in_min = 12
+# trip_time_in_sec = trip_time_in_min * 60
+# print(f"Your trip will be {trip_time_in_sec} seconds long.")
+
+# --------------------------------------------------------
+# 3
+# rows = 30
+# seats_per_row = 43
+# print(f"You have {rows * seats_per_row} seats you can sell tickets for.")
+
+# ---------------------------------------------------------
+# 4
+# shelf_1 = 45
+# shelf_2 = 64
+# print(f"Shelf 2 is higher it is {shelf_2 - shelf_1} cm taller.")
+
+# ---------------------------------------------------------------
+# 5
