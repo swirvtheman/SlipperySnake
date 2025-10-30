40837e7bdb77d391155bc8baabbacd69f32b86da
Irvin Jackson <swirvtheman@gmail.com>
2025-10-30 09:05:27 +0100

    just working

---
+++ listor_list_comp_tuples.py
@@ -0,0 +1,60 @@
+# 1
+# store_list = ["milk", "bread", "butter"]
+# store_list.append('cheese')
+# store_list.insert(0, 'yohgurt')
+
+# print(store_list)
+# ------------------------------------------
+
+# 2
+# store_list = ['yohgurt', 'milk', 'bread', 'butter', 'cheese']
+# store_list.pop()
+# store_list.pop(2)
+
+# print(store_list)
+# -------------------------------------------
+
+# 3
+# list = ["apple", "bananas", "apple", "pear", "apple"]
+
+# print(list.count("apple"))
+# print(list.index("apple"))
+# --------------------------------------------
+
+# 4
+# list = ["apple", "bananas", "apple", "pear", "apple"]
+# a = list.copy()
+# list.clear()
+
+# print(f"This is the original: {list} and this is the copy of the list: {a}.")
+# ---------------------------------------------
+
+# 5
+# a)
+# fruit = ["apple", "bananas"]
+# vegies = ["carrot", "onion"]
+# print(f"{fruit + vegies}")
+
+# b)
+# fruit = ["apple", "bananas"]
+# veggies = ["carrot", "onion"]
+
+# a = veggies.copy()
+# b = fruit.copy()
+
+# print(b + a)
+# ----------------------------------------------
+
+# 6
+# people = ["Zara", "adam", "Bea", "cecilia"]
+
+# sort_up = people.copy()
+# sort_up.sort(key=str.casefold)
+
+# sort_down = people.copy()
+# sort_down.sort(key=str.casefold, reverse=True)
+
+# print(f"Original: {people}")
+# print(f"Ascending: {sort_up}")
+# print(f"Descending: {sort_down}")
+# -----------------------------------------------
